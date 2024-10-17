7cca18c195c4582589d239cc0c10d9caa7d4ec41
pankaj-gitcode <pankajaditya75@gmail.com>
2024-10-17 22:26:57 +0530

    hero component gsap config

--- iphone/src/components/Hero.jsx
+++ iphone/src/components/Hero.jsx
@@ -18,7 +18,10 @@ export const Hero = ()=>{
     useEffect(()=>{
         // add resize event to window
         window.addEventListener('resize', handleVideoSrc)
-    }, [window.innerWidth])
+        // prevent memory leak: remove eventListener if not needed
+        return ()=>
+            window.removeEventListener('resize', handleVideoSrc)
+    }, [])
 
     // using GSAP to animate text
     useGSAP(()=>{
@@ -29,21 +32,37 @@ export const Hero = ()=>{
         })
     }, [])
 
-    // 
+    // GSAP on button and subscription
+    useGSAP(()=>{
+        gsap.fromTo('#btnSub',{
+            opacity:0,
+            y:180
+        },{
+            opacity:1,
+            y:0,
+            duration:1
+        })
+    }, [])
 
     return(<>
         <section className='w-full  relative h-[calc(100vh-60px)]'>
 
             <div className='h-5/6 flex flex-col items-center justify-center'>
                     {/* ------------ IPHONE TITLE ------------ */}
-                <h1 className='iphone15Text opacity-0 text-3xl text-gray'>iPhone 15 Pro</h1>
+                <h1 className='iphone15Text opacity-0 text-3xl text-gray font-semibold'>iPhone 15 Pro</h1>
 
                 {/* ------------- VIDEO ----------- */}
-                <div>
+                <div className='w-9/12 md:w-10/12 pt-10'>
                     <video autoPlay muted playsInline={true} key={videoSrc} className='pointer-events-none'>
                         <source src={videoSrc} type='video/mp4' />
                     </video>
                 </div>
+            </div>
+
+            {/* -------------- BUTTON & SUBCRIPTION FEE -------------- */}
+            <div id='btnSub' className='flex flex-col items-center justify-center gap-5 opacity-0'>
+                <button className='bg-blue py-2 px-5 rounded-2xl '>Buy</button>
+                <h1 className='font-semibold text-xl lg:text-lg'>From $199/month or $999</h1>
             </div>
 
         </section>
