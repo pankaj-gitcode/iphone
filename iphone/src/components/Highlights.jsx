2a242033b5de3764103319ce370a1a69af776524
pankaj-gitcode <pankajaditya75@gmail.com>
2024-10-21 22:04:54 +0530

    highlight commits

--- iphone/src/components/Highlights.jsx
+++ iphone/src/components/Highlights.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { hightlightsSlides } from '../constants'
 
 export const Highlights = ()=>{
     return(<div>
-        this is Highlights page
+        <div>
+            {
+                hightlightsSlides.map(elem=>
+                <div key={elem.id}>
+                    <p>{elem.video}</p>
+                </div>)
+            }
+        </div>
     </div>)
 }
--- iphone/src/components/Navbar.jsx
+++ iphone/src/components/Navbar.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { appleImg, bagImg, searchImg } from '../utils'
 import { navLists } from '../constants'
 
 
 export const Navbar = () => {
+   const exRef = useRef('hey'); 
+   console.log(exRef.current)
   return (
     <div>
         <header className=' w-full flex items-center justify-between py-5 px-6 sm:px-10 max-w-[1120]'>
             {/* ---------- APPLE LOGO ------------- */}
-            <div>
-                <img  src={appleImg} alt="appleLogo"  width='18' height='18'/>
+            <div ref={exRef} id='h'>
+                <img src={appleImg} alt="appleLogo"  width='18' height='18'/>
             </div>
              {/* ---------- SERVICES ------------- */}
              <div className='flex flex-1 items-center justify-center gap-6 max-sm:hidden'>
