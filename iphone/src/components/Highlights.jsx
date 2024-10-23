8eb71877e6f6e936f76ef053e096c035ce690ca5
pankaj-gitcode <pankajaditya75@gmail.com>
2024-10-23 22:30:58 +0530

    video highlight  updated

--- iphone/src/components/Highlights.jsx
+++ iphone/src/components/Highlights.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import { hightlightsSlides } from '../constants'
+import React, { useRef } from 'react'
+import { VideoCarousel } from './VideoCarousel';
 
 export const Highlights = ()=>{
+    const exRef = useRef();
+    console.log("exRef: ", exRef)
     return(<div>
-        <div className='bg-[#101010] py-28 pl-6 w-full '>
+        <div ref={el=>console.log('el: ',el)}  className='bg-[#101010] py-28 pl-6 w-full '>
             <h1 className='text-3xl text-gray font-semibold'>Get the highlights.</h1>
         </div>
+        <VideoCarousel />
     </div>)
 }
--- iphone/src/components/VideoCarousel.jsx
+++ iphone/src/components/VideoCarousel.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { hightlightsSlides } from '../constants'
 
 export const VideoCarousel = () => {
   return (<>
     <div>
-        
+        {
+            hightlightsSlides.map((list,i)=>
+            <div id='slider' key={list.id}>
+                <div className='video-carousel_container'>
+                    <div></div>
+                </div>
+            </div>
+            )
+        }
     </div>
   </> )
 }
