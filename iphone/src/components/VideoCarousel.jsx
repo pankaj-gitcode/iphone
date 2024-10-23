f088e5b6931768ae75f95e4e85f1252e422e138c
pankaj-gitcode <pankajaditya75@gmail.com>
2024-10-24 02:29:09 +0530

    video carousel updated-2

--- iphone/src/components/VideoCarousel.jsx
+++ iphone/src/components/VideoCarousel.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { hightlightsSlides } from '../constants'
 
 export const VideoCarousel = () => {
+    // useRefs
+    const videoRef = useRef([]);
+    const videoSpanRef = useRef([]);
+    const videoDivRef = useRef([]);
+
+    // initial video setups
+    const [video, setVideo] = useState({
+        videoId:0,startPlay:false, isPlaying:false, isEnd:false, 
+        isLastVideo:false
+    })
   return (<>
     <div className='flex items-center'>
         {
@@ -9,9 +19,8 @@ export const VideoCarousel = () => {
                 <div key={list.id} id='slider' className='sm:pr-20 pr-10'>
                     <div className='relative sm:w-[70vw] w-[88vw] md:h-[70vh] sm:h-[50vh] h-[35vh]'>
 
-
+                            {/* ------- VIDEO DIV ------- */}
                         <div className='w-full h-full flex items-center justify-center bg-black rounded-3xl '>
-                            
                             <video 
                             autoPlay 
                             muted 
@@ -20,6 +29,12 @@ export const VideoCarousel = () => {
                             >
                                 <source src={list.video} type='video/mp4'/>
                             </video>
+                        </div>
+                        {/* -------- TEXT ON VIDEO ---------- */}
+                        <div className='absolute top-12 left-10 text-xl md:text-2xl sm:text-xl font-medium z-10'>
+                            {list.textLists.map((elem, i)=>(
+                                <p key={i}>{elem}</p>
+                            ))}
                         </div>
                     </div>
 
